4ed9769e093b5fe55b7fb8c464d0ed91eac4e05e
Hunor <debreczenihunor@outlook.com>
2020-03-27 02:07:45 +0200

    Almost done

--- a1.c
+++ a1.c
@@ -38,18 +38,21 @@ pArguments parseArgumentsList(int argc, char **argv);
 
 int listFiles(pArguments arguments);
 
-pHeader parseHeader(pArguments arguments);
+pHeader parseHeader(char *path, int showError);
 
 void printHeader(pHeader header);
 
-char *extractLine(pHeader header, pArguments arguments);
+int extractLine(pHeader header, char *path, int section_nr, int line_nr);
+
+int findAll(pArguments arguments);
 
 int main(int argc, char **argv) {
     if (argc >= 2) {
+        pArguments arguments = parseArgumentsList(argc, argv);
         if (!strcmp(argv[1], "variant")) {
             printf("82417\n");
         } else if (!strcmp(argv[1], "list")) {
-            pArguments arguments = parseArgumentsList(argc, argv);
+
             if (arguments == NULL) {
                 printf("ERROR\nInvalid arguments\n");
                 exit(1);
@@ -58,33 +61,41 @@ int main(int argc, char **argv) {
             }
             listFiles(arguments);
         } else if (!strcmp(argv[1], "parse")) {
-            pArguments arguments = parseArgumentsList(argc, argv);
             if (arguments == NULL) {
                 printf("ERROR\nInvalid arguments\n");
                 exit(1);
             }
-            pHeader header = parseHeader(arguments);
+            pHeader header = parseHeader(arguments->path, 1);
             if (header != NULL) {
                 printHeader(header);
             }
         } else if (!strcmp(argv[1], "extract")) {
-            pArguments arguments = parseArgumentsList(argc, argv);
             if (arguments == NULL) {
                 printf("ERROR\nInvalid arguments\n");
                 exit(1);
             }
-            pHeader header = parseHeader(arguments);
-            char *line = extractLine(header, arguments);
-            if (line != NULL) {
-                printf("SUCCESS\n%s", line);
-            }
+            pHeader header = parseHeader(arguments->path, 0);
+            extractLine(header, arguments->path, arguments->sect_nr, arguments->line_nr);
+//            if (line != NULL) {
+//                printf("SUCCESS\n%s\n", line);
+//            }
         } else if (!strcmp(argv[1], "findall")) {
-            printf("SUCCESS");
-        } else if (!strcmp(argv[1], "parse")) {
-
+            printf("SUCCESS\n");
+            findAll(arguments);
         }
+        free(arguments);
     }
     return 0;
+}
+
+int findAll(pArguments arguments) {
+    if (arguments->no_of_options == 0) {
+        arguments->options = (char **) malloc(sizeof(char *));
+    }
+    arguments->options[arguments->no_of_options++] = "sections_smaller=910";
+    arguments->recursive = 1;
+    listFiles(arguments);
+    return 1;
 }
 
 pArguments parseArgumentsList(int argc, char **argv) {
@@ -115,22 +126,23 @@ pArguments parseArgumentsList(int argc, char **argv) {
 }
 
 int listFiles(pArguments arguments) {
-    if (arguments->path[strlen(arguments->path) - 1] != '/') {
-        strcat(arguments->path, "/");
-    }
-
     DIR *dir;
     struct dirent *dirEntry;
     struct stat inode;
     char path[MAX_PATH_LEN];
     char folder[MAX_PATH_LEN];
-    strcpy(folder, arguments->path);
 
-    dir = opendir(folder);
+
+    dir = opendir(arguments->path);
     if (dir == NULL) {
         printf("ERROR\ninvalid directory path\n");
         exit(1);
     }
+
+    if (arguments->path[strlen(arguments->path) - 1] != '/') {
+        strcat(arguments->path, "/");
+    }
+    strcpy(folder, arguments->path);
 
     while ((dirEntry = readdir(dir)) != 0) {
         char *name = dirEntry->d_name;
@@ -138,19 +150,19 @@ int listFiles(pArguments arguments) {
             continue;
         }
 
-        snprintf(path, MAX_PATH_LEN, "%s%s", folder, name);
+        sprintf(path, "%s%s", folder, name);
         lstat(path, &inode);
 
-        if (S_ISDIR(inode.st_mode) && arguments->recursive) {
-            strcpy(arguments->path, path);
-            strcat(arguments->path, "/");
+        if (dirEntry->d_type == DT_DIR && arguments->recursive) {
+            sprintf(arguments->path, "%s/", path);
             listFiles(arguments);
         }
         int good = 1;
-        for (int i = 0; i < arguments->no_of_options; i++) {
-            char *value = (char *) malloc(sizeof(arguments->options[i]));
-            strcpy(value, arguments->options[i]);
-            char *option = strtok_r(value, "=", &value);
+        for (int i = 0; i < arguments->no_of_options && good; i++) {
+            char valueOriginal[strlen(arguments->options[i]) + 1];
+            strcpy(valueOriginal, arguments->options[i]);
+            char *option = strtok(valueOriginal, "=");
+            char *value = strtok(NULL, "=");
             if (!strcmp(option, "size_smaller")) {
                 if (inode.st_size >= atoi(value) || !S_ISREG(inode.st_mode)) {
                     good = 0;
@@ -161,57 +173,86 @@ int listFiles(pArguments arguments) {
                     good = 0;
                 }
             }
-//            free(value);
-            free(option);
+            if (!strcmp(option, "sections_smaller")) {
+                if (!S_ISREG(inode.st_mode)) {
+                    good = 0;
+                } else {
+                    pHeader header = parseHeader(path, 0);
+                    if (header == NULL) {
+                        good = 0;
+                    }
+                    for (int nrSection = 0; header != NULL && nrSection < header->no_of_sections && good; nrSection++) {
+                        if (header->pSections[nrSection]->size > 910) {
+                            good = 0;
+                        }
+                    }
+                }
+            }
         }
         if (good) {
             printf("%s\n", path);
         }
     }
+    free(dirEntry);
     closedir(dir);
     return 1;
 }
 
-pHeader parseHeader(pArguments arguments) {
+pHeader parseHeader(char *path, int showError) {
     int fd;
-    if (access(arguments->path, R_OK) == -1) {
-        chmod(arguments->path, S_IRUSR | S_IRGRP | S_IROTH);
+    if (access(path, R_OK) == -1) {
+        chmod(path, S_IRUSR | S_IRGRP | S_IROTH);
     }
-    if ((fd = open(arguments->path, O_RDONLY, 0644)) < 0) {
+    if ((fd = open(path, O_RDONLY, 0644)) < 0) {
         printf("ERROR\nUnable to open file\n");
         exit(1);
     }
 
     pHeader header = (pHeader) malloc(sizeof(Header));
+    memset(header, '\0', sizeof(Header));
+
     if (read(fd, header->magic, sizeof(header->magic)) < 0) {
         printf("ERROR\nUnable to read\n");
         return NULL;
     }
-    if (strcmp(header->magic, "79ws")) {
-        printf("ERROR\nwrong magic\n");
+    if (strcmp(header->magic, "79ws") != 0) {
+        if (showError) {
+            printf("ERROR\nwrong magic\n");
+        }
         return NULL;
     }
 
     if (read(fd, &header->size, sizeof(header->size)) < 0) {
-        printf("ERROR\nUnable to read\n");
+        if (showError) {
+            printf("ERROR\nUnable to read\n");
+        }
+
         return NULL;
     }
 
     if (read(fd, &header->version, sizeof(header->version)) < 0) {
-        printf("ERROR\nUnable to read\n");
+        if (showError) {
+            printf("ERROR\nUnable to read\n");
+        }
         return NULL;
     }
     if (!(header->version >= 83 && header->version <= 141)) {
-        printf("ERROR\nwrong version\n");
+        if (showError) {
+            printf("ERROR\nwrong version\n");
+        }
         return NULL;
     }
 
     if (read(fd, &header->no_of_sections, sizeof(header->no_of_sections)) < 0) {
-        printf("ERROR\nUnable to read\n");
+        if (showError) {
+            printf("ERROR\nUnable to read\n");
+        }
         return NULL;
     }
     if (!(header->no_of_sections >= 5 && header->no_of_sections <= 10)) {
-        printf("ERROR\nwrong sect_nr\n");
+        if (showError) {
+            printf("ERROR\nwrong sect_nr\n");
+        }
         return NULL;
     }
 
@@ -220,27 +261,37 @@ pHeader parseHeader(pArguments arguments) {
         pSection section = (pSection) malloc(sizeof(Section));
 
         if (read(fd, section->name, sizeof(section->name) - 1) < 0) {
-            printf("ERROR\nUnable to read\n");
+            if (showError) {
+                printf("ERROR\nUnable to read\n");
+            }
             return NULL;
         }
         strcat(section->name, "\0");
 
         if (read(fd, &section->type, sizeof(section->type)) < 0) {
-            printf("ERROR\nUnable to read\n");
+            if (showError) {
+                printf("ERROR\nUnable to read\n");
+            }
             return NULL;
         }
         if (!(section->type == 17 || section->type == 18)) {
-            printf("ERROR\nwrong sect_types\n");
+            if (showError) {
+                printf("ERROR\nwrong sect_types\n");
+            }
             return NULL;
         }
 
         if (read(fd, &section->offset, sizeof(section->offset)) < 0) {
-            printf("ERROR\nUnable to read\n");
+            if (showError) {
+                printf("ERROR\nUnable to read\n");
+            }
             return NULL;
         }
 
         if (read(fd, &section->size, sizeof(section->size)) < 0) {
-            printf("ERROR\nUnable to read\n");
+            if (showError) {
+                printf("ERROR\nUnable to read\n");
+            }
             return NULL;
         }
 
@@ -263,57 +314,63 @@ void printHeader(pHeader header) {
     }
 }
 
-char *extractLine(pHeader header, pArguments arguments) {
+int extractLine(pHeader header, char *path, int section_nr, int line_nr) {
     ///Check if File can be opened
     int fd;
-    if (access(arguments->path, R_OK) == -1) {
-        chmod(arguments->path, S_IRUSR | S_IRGRP | S_IROTH);
+    if (access(path, R_OK) == -1) {
+        chmod(path, S_IRUSR | S_IRGRP | S_IROTH);
     }
-    if ((fd = open(arguments->path, O_RDONLY, 0644)) < 0) {
+    if ((fd = open(path, O_RDONLY, 0644)) < 0) {
         printf("ERROR\ninvalid file\n");
         exit(1);
     }
 
     ///Check if section exists
-    if (header->no_of_sections < arguments->sect_nr) {
+    if (header->no_of_sections < section_nr) {
         printf("ERROR\ninvalid section");
-        return NULL;
+        return 0;
     }
-    int offset = header->pSections[arguments->sect_nr - 1]->offset;
-    int size = header->pSections[arguments->sect_nr - 1]->size;
+    int offset = header->pSections[section_nr - 1]->offset;
+    int size = header->pSections[section_nr - 1]->size;
 
-    char *line = (char *) malloc(BUF_SIZE);
+//    char *line = (char *) malloc(BUF_SIZE);
     int currentLine = 1;
 
-    char buf[2];
-    buf[1] = '\0';
+    char buf = '\0';
     char bufPrev = '\r';
     lseek(fd, offset + --size, SEEK_SET);
-    read(fd, &buf[0], 1);
+    read(fd, &buf, 1);
     do {
         lseek(fd, offset + --size, SEEK_SET);
 
-        if (!(buf[0] == '\n' || buf[0] == '\r')) {
-            strcat(line, buf);
-        }
-
-        if (bufPrev == '\n' && buf[0] == '\r') {
-            strcat(line, "\0");
-            if (arguments->line_nr == currentLine++) {
-                return line;
+//        if (!(buf[0] == '\n' || buf[0] == '\r')) {
+//            strcat(line, buf);
+//        }
+        if (line_nr == currentLine) {
+            if (bufPrev == '\r') {
+                printf("SUCCESS\n");
             }
-            memset(line, '\0', BUF_SIZE);
-            continue;
+            printf("%c", buf);
         }
-        bufPrev = buf[0];
-    } while (read(fd, &buf[0], 1) != -1 && size > -1);
 
-    strcat(line, "\0");
-    if (arguments->line_nr == currentLine) {
-        return line;
+        if (bufPrev == '\n' && buf == '\r') {
+//            strcat(line, "\0");
+            if (line_nr == currentLine++) {
+                return 1;
+            }
+//            memset(line, '\0', BUF_SIZE);
+//            continue;
+        }
+        bufPrev = buf;
+        buf = '\0';
+    } while (read(fd, &buf, 1) != -1 && size > -1);
+    if (line_nr == currentLine) {
+        if (buf != '\0') {
+            printf("%c", buf);
+        }
+        return 1;
     }
 
     printf("ERROR\ninvalid line");
-    return NULL;
+    return 0;
 }
-
